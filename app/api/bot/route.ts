eff93d26c25fe774a13d8f1d53655e03e3561ab6
slstkhn <denis.l.tron@ya.ru>
2025-04-20 01:22:29 +0300

    Update route.ts

--- app/api/bot/route.ts
+++ app/api/bot/route.ts
@@ -7,17 +7,19 @@ if (!BOT_TOKEN) throw new Error('BOT_TOKEN env not set');
 const bot = new Telegraf(BOT_TOKEN);
 
 // приветствие на /start
+const WEBAPP_URL = 'https://metro-telegram-miniapp-jibf.vercel.app';
+
 bot.start((ctx) =>
   ctx.reply(
     'Привет! Я помогу построить быстрый маршрут в метро.\n' +
-    'Нажмите кнопку «Открыть» ниже, чтобы запустить мини‑приложение.',
+      'Нажмите кнопку «Открыть» ниже, чтобы запустить мини‑приложение.',
     {
       reply_markup: {
         inline_keyboard: [
           [
             {
               text: 'Открыть приложение',
-              web_app: { url: metro-telegram-miniapp-jibf.vercel.app }   // <- магия
+              web_app: { url: WEBAPP_URL }
             }
           ]
         ]
@@ -25,6 +27,7 @@ bot.start((ctx) =>
     }
   )
 );
+
 
 // инициализируем Telegraf один раз (важно для serverless)
 let inited = false;
