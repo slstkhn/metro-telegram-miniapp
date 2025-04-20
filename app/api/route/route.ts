2687af5941792e4d234a58a3a5b5e82b216ea7d6
slstkhn <denis.l.tron@ya.ru>
2025-04-20 22:34:43 +0300

    Update route.ts

--- app/api/route/route.ts
+++ app/api/route/route.ts
@@ -31,5 +31,5 @@ export async function POST(req: NextRequest) {
     segments.push({ from: sFrom.name, to: sTo.name, minutes, wagonsTip: wagon });
   }
 
-  return NextResponse.json({ total: dist, segments });
+  return NextResponse.json({ total: dist[to], segments });
 }
